92cccc8358ea8bee111782ade315d1cbda61bc08
derwind <uncertainty_principle_ss@yahoo.co.jp>
2017-07-31 20:19:45 +0900

    a script which appends empty layers to specified glyphs

---
+++ append_empty_layers.py
@@ -0,0 +1,26 @@
+#MenuTitle: Append Emoty Layers
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Create empty layers to specified glyphs.
+"""
+
+target_cids = range(842, 924+1)
+
+def empty_layer(g):
+    newLayer = g.layers[0].copy()
+    for i in reversed(range(len(newLayer.paths))):
+        del newLayer.paths[i]
+    return newLayer
+
+def main():
+    glyphs = Glyphs.font.glyphs
+    for cid in target_cids:
+        gname = "cid{0:05d}".format(cid)
+        g = glyphs[gname]
+        newLayer = empty_layer(g)
+        newLayer.name = "{}_stroke".format(g.name)
+        g.layers.append(newLayer)
+
+if __name__ == "__main__":
+    main()
